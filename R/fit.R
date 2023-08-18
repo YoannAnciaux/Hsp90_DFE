acaa8128ecb4b2c9704c98d972e2f5e01a2d1a12
YoannAnciaux <anciaux.yoann@gmail.com>
2023-08-19 00:22:34 +0200

    Comment fit script

--- R/fit.R
+++ R/fit.R
@@ -9,7 +9,7 @@ library(cowplot)
 library(gridExtra)
 source(here("R","dfe_functions.R"))
 
-
+#### Import data and compute statistics for fit ####
 data_DFE <- read_delim(here("Data", "database_clusters_reduced.csv"), delim = ";",
                        col_types = "_fffdddff",
                        locale=locale(decimal_mark = ","))
@@ -18,7 +18,7 @@ data_DFE <- data_DFE %>%
   filter(AA != "WILDTYPE") %>% # remove the wildtype as we want ony mutants for the DFE
   mutate(sd = abs(Upper_CI - Low_CI) / 4) #approximation of the standard deviation by the standard error.
 
-# Count of mutationsin each environment
+# Count of mutations in each environment
 stats_all <- data_DFE %>%
   group_by(Environment) %>%
   summarise(number = n())
@@ -38,20 +38,28 @@ par_ini_norm <- left_join(stats_stop, stats_all) %>%
             sd = sd_stop,
             prob = 1-number_stop/number)
 
-
-
-
-
 envs <- data_DFE$Environment %>% unique() %>% as.vector()
 names(envs) <- envs
+
+#### MGE fit ####
+# Fit DFE of all AA in each environment using a goodness of fit method based on
+# the Cramer-von Mises distance. It gives results which are more plausible than the
+# MLE method for the number of dimension in both 37C and Diamide environemnts. The
+# absolute value of the parameters is a bit different between MLE and MGE for the
+# other environments but the relative values of these parameters between environments
+# stay similar.
+# We fix the mean, and the sd of the gaussian to the mean and sd of the codon stops
+# selection coefficients to reduce the number of parameters to fit.
+# Here we use a mixture of the FGM DFE and a gaussian but lambda is not fitted, it
+# is proportionnal to the n_dim and the mean of the data which are considered not
+# in the gaussian.
 mgefit <- sapply(envs,
                  function(e) {
                    print(e)
                    par_ini_norm_env <- filter(par_ini_norm, Environment == e);
                    data_env <- filter(data_DFE, Environment == e)$s
-                   weights_env <- floor(1 / (filter(data_DFE, Environment == e)$sd)^2)
                    
-                   fit_env <- fitdist(data_env, "normfgmsmix", method = "mge", optim.method = "default", #weights = weights_env,
+                   fit_env <- fitdist(data_env, "normfgmsmix", method = "mge", optim.method = "default",
                                       start = list(n_dim = 2, so = max(data_env)*1.001, prob = par_ini_norm_env$prob),
                                       fix.arg = list(mean = par_ini_norm_env$mean, sd = par_ini_norm_env$sd, mean_data = mean(data_env)),
                                       lower = c(1., 0., (1-mean(data_env)/par_ini_norm_env$mean)*1.001), upper = c(10000., 10000., 1.))
@@ -73,36 +81,9 @@ mgefit <- sapply(envs,
 
 saveRDS(mgefit, here("Analysis", "mgefit_alldata_scaledlambda.rds"))
 
-sapply(envs, function(e) mgefit[[e]]$fit$estimate, simplify = T, USE.NAMES = T)
 
-tbl_dfe <- sapply(envs, function(e) mgefit[[e]]$dfe, simplify = F, USE.NAMES = F) %>% bind_rows()
-
-p1 <- ggplot() +
-  geom_histogram(aes(x = s, y = ..density.., color = "1"), bins = 50, data = data_DFE) +
-  geom_line(aes(x = s, y = density, color = "2"), data = tbl_dfe) +
-  facet_wrap(~Environment) +
-  xlab("selection coefficients") +
-  scale_color_manual(name = "DFE", breaks = c("1","2"), labels = c("data", "Fit mixture FGM & Normal"), values = c("#808080","#FF0000"))+
-  theme(legend.position = c(0.2, 0.85))
-
-t1 <- sapply(envs, function(e) mgefit[[e]]$fit$estimate, simplify = T, USE.NAMES = T) %>%
-  t() %>%
-  round(digits = 2) %>%
-  as.data.frame() %>%
-  tableGrob(theme = ttheme_default(
-    colhead = list(fg_params=list(col="black"),
-                   bg_params=list(fill="grey")),
-    rowhead = list(fg_params=list(col="black", fontface="bold",hjust=0.5, x=0.5),
-                   bg_params=list(fill="grey"))))
-
-
-
-plot_grid(p1, t1, rel_widths = c(3, 1))
-
-
-
-
-### mle fitting does no reach acceptable n_dim maybe because of lambda constrained.
+#### MLE fit ####
+# mle fitting does no reach acceptable n_dim maybe because of lambda constrained.
 # mlefit <- sapply(envs,
 #                  function(e) {
 #                    print(e)
